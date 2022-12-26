4dfdfcf1f0d09989f6d4c05e7534bce533f153c6
Pongpol prommacharoen <konse-komde@hotmail.com>
2022-12-26 17:10:06 +0700

    Updates

--- js/dom-04-removeable.js
+++ js/dom-04-removeable.js
@@ -1,52 +1,72 @@
-function calculateTotle(container, resultComponent) {
-    const inputs = [...container.querySelectorAll('input[type="number"]')];
-    const total = inputs.reduce((carry, em) => carry + em.valueAsNumber, 0);
-
+function computeTotal(inputsContainer, resultComponent) {
+    const total = [...inputsContainer.querySelectorAll('.cmp-input-container')]
+      .map((elem) => elem.querySelector('.cmp-input'))
+      .reduce((carry, elem) => carry + elem.valueAsNumber, 0);
+  
     resultComponent.value = total;
-}
-
-function add(container, resultComponent, template) {
+  }
+  
+  function rebuildIndex(inputsContainer) {
+    const inputContainers = [
+      ...inputsContainer.querySelectorAll('.cmp-input-container'),
+    ];
+  
+    inputContainers.forEach((elem, i) => {
+      [...elem.querySelectorAll('.cmp-input-no')].forEach((elem) => {
+        elem.innerText = i + 1;
+      });
+    });
+  
+    [...inputsContainer.querySelectorAll('.cmd-remove-input')].forEach((elem) => {
+      elem.disabled = !(inputContainers.length > 1);
+    });
+  }
+  
+  function add(inputsContainer, resultComponent, template) {
     const fragment = template.content.cloneNode(true);
-    const title = fragment.querySelector('.cmp-input-title');
-    const input = fragment.querySelector('input[type="number"]');
-
-    container.append(fragment);
-
-    const nextN = container.querySelectorAll('input[type="number"]').length;
-    title.innerText = `Number ${nextN}`;
-
-    calculateTotle(container, resultComponent);
-
-    input.addEventListener('change', () => {
-        calculateTotle(container, resultComponent);
-    });
-}
-
-function remove (container, resultComponent, inputContainer) {
+  
+    inputsContainer.append(fragment);
+  
+    rebuildIndex(inputsContainer);
+    computeTotal(inputsContainer, resultComponent);
+  }
+  
+  function remove(inputsContainer, resultComponent, inputContainer) {
     inputContainer.remove();
-
-    calculateTotle(container, resultComponent);
-}
-
-function assign (container, inputemplate) {
-    const inputContainer = container.querySelector('.cmp-inputs-container');
-    const resultComponent = container.querySelector('.cmp-result');
-
-    container.addEventListener('click', (ev) => {
-        if(ev.target.matches('input[type="number"]')) {
-            
-        }
+  
+    rebuildIndex(inputsContainer);
+    computeTotal(inputsContainer, resultComponent);
+  }
+  
+  function assign(inputSection, inputTemplate) {
+    const inputsContainer = inputSection.querySelector('.cmp-inputs-container');
+    const resultComponent = inputSection.querySelector('.cmp-result');
+  
+    inputSection.addEventListener('click', (ev) => {
+      if (ev.target.matches('.cmd-add-input')) {
+        add(inputsContainer, resultComponent, inputTemplate);
+      }
     });
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-    const inputsContainer = document.querySelector('.cmp-inputs-container');
-    const resultComponent = document.querySelector('output.cmp-result');
-    const template = document.querySelector('template#tmp-input');
-
-    document.querySelector('.cmd-add-input').addEventListener('click', () => {
-        add(inputsContainer, resultComponent, template);
+  
+    inputsContainer.addEventListener('change', (ev) => {
+      if (ev.target.matches('input[type="number"]')) {
+        computeTotal(inputsContainer, resultComponent);
+      }
     });
-    
-    add(inputsContainer, resultComponent, template);
-});
+  
+    inputsContainer.addEventListener('click', (ev) => {
+      if (ev.target.matches('.cmd-remove-input')) {
+        const inputContainer = ev.target.closest('.cmp-input-container');
+        remove(inputsContainer, resultComponent, inputContainer);
+      }
+    });
+  
+    add(inputsContainer, resultComponent, inputTemplate);
+  }
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    const inputTemplate = document.querySelector('template#tmp-input');
+    const inputSection = document.querySelector('.cmp-input-section');
+  
+    assign(inputSection, inputTemplate);
+  });
